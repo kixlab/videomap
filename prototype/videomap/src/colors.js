d6ca02e0579ee9a29d2244a3dcb8db98fe8ccc16
saelyne <saelyne@kaist.ac.kr>
2022-08-30 15:50:31 +0900

    separated color info

---
+++ prototype/videomap/src/colors.js
@@ -0,0 +1,45 @@
+export const colorPalette = {
+    // high-level type
+    'greeting':'yellow',
+    'overview':'green ',
+    'step':'purple',
+    'supplementary':'white',
+    'explanation':'lightblue',
+    'description':'yellow',
+    'conclusion':'blue',
+    'misc.':'lightcyan',
+    
+    // low-level type
+    //greeting
+    'opening':'#DAF283',
+    //overview
+    'goal': '#F6BD60',
+    'motivation':'#F6BD60',
+    'briefing':'#F6BD60',
+    //step
+    'subgoal':'#F5A7A6 ',
+    'instruction':'#F5A7A6',
+    'tool':'#F5A7A6',
+    'tool (multiple)':'#F5A7A6',
+    'tool (optional)':'#F5A7A6',
+    //explanation
+    'justification':'#FFF5AB',
+    'effect':'#FFF5AB',
+    //supplementary
+    'warning':'#C7B4C4',
+    'tip':'#C7B4C4',
+    //description
+    'status':'#A8D0C6',
+    'context':'#A8D0C6',
+    'tool spec.':'#A8D0C6',
+    //greeting-outro
+    'closing':'#DAF283',
+    //conclusion
+    'outcome':'#DCC8E6',
+    'reflection':'#DCC8E6',
+    //misc
+    'side note':'lightgray',
+    'self-promo':'lightgray',
+    'bridge':'lightgray',
+    'filler':'lightgray'
+}
