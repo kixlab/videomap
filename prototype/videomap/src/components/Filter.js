4cb7e99e829d37513c5050c5ad509f9defc8402e
saelyne <saelyne@kaist.ac.kr>
2022-08-31 17:31:53 +0900

    filter hierarchy included

--- prototype/videomap/src/components/Filter.js
+++ prototype/videomap/src/components/Filter.js
@@ -6,112 +6,140 @@ import './Filter.css';
 const Filter = ({colorPalette}) => {
     return (
         <div className="filter_wrapper">
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['greeting']}}>
-                    Greeting
+            <div className="category"> 
+                <div className="category_label">
+                    Intro
                 </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['greeting']}}>
-                    Opening
+                <div className="class">
+                    <div className="group">
+                        <div className="group_label">
+                            Greeting
+                        </div>
+                        <div className="label" style={{backgroundColor: colorPalette['greeting']}}>
+                            Opening
+                        </div>
+                    </div>
+                    <div className="group">
+                        <div className="group_label">
+                            Overview
+                        </div>
+                        <div className="label" style={{backgroundColor: colorPalette['overview']}}>
+                            Goal
+                        </div>
+                        <div className="label" style={{backgroundColor: colorPalette['overview']}}>
+                            Motivation
+                        </div>
+                        <div className="label" style={{backgroundColor: colorPalette['overview']}}>
+                            Briefing
+                        </div>
+                    </div>
+                </div>          
+            </div>
+            <div className="category"> 
+                <div className="category_label">
+                    Procedure
+                </div>
+                <div className="class">          
+                <div className="group">
+                    <div className="group_label">
+                        Step
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['step']}}>
+                        Subgoal
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['step']}}>
+                        Instruction
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['step']}}>
+                        Tool
+                    </div>
+                </div>
+                <div className="group">
+                    <div className="group_label">
+                        Supplementary
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['supplementary']}}>
+                        Tip
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['supplementary']}}>
+                        Warning
+                    </div>
+                </div>
+                <div className="group">
+                    <div className="group_label">
+                        Explanation
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['explanation']}}>
+                        Justification
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['explanation']}}>
+                        Effect
+                    </div>
+                </div>
+                <div className="group">
+                    <div className="group_label">
+                        Description
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['description']}}>
+                        Status
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['description']}}>
+                        Context
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['description']}}>
+                        Tool Spec.
+                    </div>
+                </div>
                 </div>
             </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['overview']}}>
-                    Overview
+            <div className="category"> 
+                <div className="category_label">
+                    Outro
                 </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['overview']}}>
-                    Goal
+                <div className="class">       
+                <div className="group">
+                    <div className="group_label">
+                        Greeting
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['greeting']}}>
+                        Closing
+                    </div>
                 </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['overview']}}>
-                    Motivation
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['overview']}}>
-                    Briefing
-                </div>
-            </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['step']}}>
-                    Step
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['step']}}>
-                    Subgoal
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['step']}}>
-                    Instruction
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['step']}}>
-                    Tool
+                <div className="group">
+                    <div className="group_label">
+                        Conclusion
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['conclusion']}}>
+                        Outcome
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['conclusion']}}>
+                        Reflection
+                    </div>
                 </div>
             </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['supplementary']}}>
-                    Supplementary
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['supplementary']}}>
-                    Tip
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['supplementary']}}>
-                    Warning
-                </div>
             </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['explanation']}}>
-                    Explanation
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['explanation']}}>
-                    Justification
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['explanation']}}>
-                    Effect
-                </div>
-            </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['description']}}>
-                    Description
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['description']}}>
-                    Status
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['description']}}>
-                    Context
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['description']}}>
-                    Tool Specification
-                </div>
-            </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['greeting']}}>
-                    Greeting
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['greeting']}}>
-                    Closing
-                </div>
-            </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['conclusion']}}>
-                    Conclusion
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['conclusion']}}>
-                    Outcome
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['conclusion']}}>
-                    Reflection
-                </div>
-            </div>
-            <div className="filter_group">
-                <div className="filter_high_label" style={{backgroundColor: colorPalette['misc']}}>
+            <div className="category"> 
+                <div className="category_label">
                     Misc.
                 </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['misc']}}>
-                    Side Note
+                <div className="class">
+                <div className="group">
+                    <div className="group_label">
+                        Misc.
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['misc']}}>
+                        Side Note
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['misc']}}>
+                        Self-promotion
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['misc']}}>
+                        Bridge
+                    </div>
+                    <div className="label" style={{backgroundColor: colorPalette['misc']}}>
+                        Filler
+                    </div>
                 </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['misc']}}>
-                    Self-promotion
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['misc']}}>
-                    Bridge
-                </div>
-                <div className="filter_label" style={{backgroundColor: colorPalette['misc']}}>
-                    Filler
                 </div>
             </div>
         </div>
